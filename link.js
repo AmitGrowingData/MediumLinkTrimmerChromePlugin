bc037d53987900cc26d3b0c780fbc28336a9aee1
Amit <amit@growingdata.com.au>
2019-11-20 10:54:24 +1100

    Addec code to open link in incognoto window instead of just as a new tab

--- link.js
+++ link.js
@@ -1,11 +1,12 @@
-
 function getword(info,tab) {
 	originalLink=info.linkUrl
   	indexOfQuestion=originalLink.indexOf("?")
   	newURL= originalLink.substring(0,indexOfQuestion)
-  	chrome.tabs.create({  
-    url: newURL
-  });
+  // 	chrome.tabs.create({  
+  //   url: newURL
+  // });
+  //opening in an incognito window to avoid further tracking
+  chrome.windows.create({"url": newURL, "incognito": true});
 
 }
 
